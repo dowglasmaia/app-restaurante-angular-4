b927dfe016cf79768357af557c29c2f5a38c7c23
dowglasmaia <dowglasmaia@gmail.com>
2021-03-17 22:38:52 -0300

    Criando componente de Restaurante

--- src/app/restaurantes/restaurante/restaurante.component.ts
+++ src/app/restaurantes/restaurante/restaurante.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Restaurante } from './Restaurante.model';
 
 @Component({
   selector: 'mt-restaurante',
-  templateUrl: './restaurante.component.html',
-  styleUrls: ['./restaurante.component.css']
+  templateUrl: './restaurante.component.html'
 })
 export class RestauranteComponent implements OnInit {
 
+  @Input() restaurante: Restaurante;
+   
   constructor() { }
 
   ngOnInit() {
--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -1,11 +1,13 @@
+import { Restaurante } from './restaurante/Restaurante.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mt-restaurantes',
-  templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css']
+  templateUrl: './restaurantes.component.html'
 })
 export class RestaurantesComponent implements OnInit {
+
+  restaurantes: Restaurante[] = [];
 
   constructor() { }
 
