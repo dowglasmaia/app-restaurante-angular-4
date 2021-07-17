3914b03d3bcf4bae48b9760c8189fb33e68c6dcf
Dowglas Maia <dowglasmaia@gmail.com>
2021-07-17 19:18:36 -0300

    Impl tratative de Error

--- src/app/restaurantes/restaurentes.service.ts
+++ src/app/restaurantes/restaurentes.service.ts
@@ -3,9 +3,11 @@ import { Http } from "@angular/http";
 import { URI_SERVER_API } from "app/app.api";
 
 import { Restaurante } from "./restaurante/Restaurante.model";
+import { ErrorHandler } from "app/app.error-handler";
 
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 @Injectable()
 export class RestauranteServices {
@@ -13,6 +15,7 @@ export class RestauranteServices {
 
   obterRestaurantes(): Observable<Restaurante[]> {
     return this.http.get(`${URI_SERVER_API}/restaurants`)
-      .map((response) => response.json());
+      .map((response) => response.json())
+      .catch(ErrorHandler.handlerError);
   }
 }
